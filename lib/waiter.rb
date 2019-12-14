f4ce24a6508f92a280ee48f5f651ef1f648c584c
Brian Nicholls <nichol88@gmail.com>
2019-12-14 04:22:55 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,6 +22,6 @@ class Waiter
   end
 
   def best_tipper
-    self.meals.inject(0){|memo, meal| meal.tip > memo}
+    self.meals.inject(0){|memo, meal| meal.tip = memo if memo < meal.tip}
   end
 end
