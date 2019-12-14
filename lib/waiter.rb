31b0f48959b2bd17cefda5c25966e4ec3dfe81a9
Brian Nicholls <nichol88@gmail.com>
2019-12-14 04:07:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
+  attr_accessor :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
 end
