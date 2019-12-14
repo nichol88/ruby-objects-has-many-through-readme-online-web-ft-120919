7697605342a177927dc409f89a9571a22d508484
Brian Nicholls <nichol88@gmail.com>
2019-12-14 04:33:28 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,4 +6,24 @@ class Customer
     @age = age
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    self.meals.collect do |meal|
+      meal.waiter
+    end
+  end
 end
